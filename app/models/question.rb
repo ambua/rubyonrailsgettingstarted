36e7786fce7c425c234e95c27878c5ff5c5a3334
bodabie <you@example.com>
2018-05-25 09:03:39 +0200

    Create new question implemented

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+    def gravatar
+        #"http.//www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+        "http://www.gravatar.com/avatar/424242"
+    end
 end
